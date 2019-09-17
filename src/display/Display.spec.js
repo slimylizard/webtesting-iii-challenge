2bf38199db1abcaf3ed0d6e21dc8c1a2d0dd2765
ian-sanchez <silkyhairbear@gmail.com>
2019-09-17 08:41:24 -0700

    ian-sanchez

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display';
+
+describe('<App />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Display />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
